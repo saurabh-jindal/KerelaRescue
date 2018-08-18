eb3ab80599fa91ca40b2bd1730335ac0d066ef73
saurabh-jindal <jindalsaurahb533@gmail.com>
2018-08-18 22:11:33 +0530

    Superusercreated

--- rescue/views.py
+++ rescue/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return HttpResponse('You are on my side!')
+    return render(request, rescue/index.html, {})
