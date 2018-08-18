312a7e10b0c8a3be9d21cd95f97a010bbc04e7d7
saurabh-jindal <jindalsaurahb533@gmail.com>
2018-08-18 22:20:23 +0530

    Initial Templates

--- rescue/views.py
+++ rescue/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return render(request, rescue/index.html, {})
+    return render(request, "rescue/index.html", {})
